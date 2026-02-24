d9eafdbfbdda18d08641dd4f7550bd2459f75b47
Rodrigo CG <rchuquimia94@gmai.com>
2026-02-24 13:06:05 -0400

    crud de users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'user_crud',
+      password: 'abc123',
+      database: 'db_practica',
+      autoLoadEntities: true,
+      synchronize: true, // Que se migre automaticamente solo en desarrollo si ya se tien una base de datos
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/users/users.service.ts
@@ -0,0 +1,43 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+    const existEmail = await this.userRepository.findOne({ where: { email: createUserDto.email } });
+    if (existEmail) throw new BadRequestException('El email ya esta registrado')
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
+  }
+
+  async findAll() {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: number) {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) throw new NotFoundException(`El usuario con el id ${id} no existe`)
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const result = await this.userRepository.update(id, updateUserDto);
+    if (result.affected === 0) throw new NotFoundException(`El usuario con el id ${id} no existe`);
+    return this.findOne(id);
+  }
+
+  async remove(id: number) {
+    const result = await this.userRepository.softDelete(id);
+    if (result.affected === 0) throw new NotFoundException(`El usuario con el id ${id} no existe`)
+    return { message: `Usuario eliminado correctamente` }
+  }
+}
